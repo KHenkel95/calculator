eeff34b0fa7dd4572f32e1b89396da1a6c702925
Ken Henkel <kenhenkel95@gmail.com>
2024-12-04 13:07:46 -0500

    Create operate function in javascript.js
    Takes an operator and two nums and calls the correct function based on
    the operator. Also created three variables for the two numbers and the
    operator

--- javascript.js
+++ javascript.js
@@ -1,3 +1,7 @@
+let firstNum;
+let secondNum;
+let operator;
+
 function add(firstNum, secondNum){
     return firstNum + secondNum;
 }
@@ -12,4 +16,21 @@ function multiply(firstNum, secondNum){
 
 function divide(firstNum, secondNum){
     return firstNum / secondNum;
+}
+
+function operate(operator, firstNum, secondNum){
+    switch(operator){
+        case '+':
+            add(firstNum, secondNum);
+            break;
+        case '-':
+            subtract(firstNum, secondNum);
+            break;
+        case '*':
+            multiply(firstNum, secondNum);
+            break;
+        case '/':
+            divide(firstNum, secondNum);
+            break;
+    }
 }
