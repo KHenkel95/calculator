5327fbc2eef9b6667acdc67e50ad6ee82825c203
Ken Henkel <kenhenkel95@gmail.com>
2024-12-05 14:47:48 -0500

    Display digits in the calculator Display
    Added functionality that displays the digits in the display that
    correspond with the digit button that was clicked by the user

--- javascript.js
+++ javascript.js
@@ -1,6 +1,17 @@
 let firstNum;
 let secondNum;
 let operator;
+const display = document.querySelector('.display');
+const buttonContainer = document.querySelector('.button-container');
+
+buttonContainer.addEventListener('click', (event) => {
+    let target = event.target;
+
+    switch(target.className){
+        case 'digit-button':
+            display.textContent += target.textContent;
+    }
+});
 
 function add(firstNum, secondNum){
     return firstNum + secondNum;
